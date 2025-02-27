32a61341a59e774223d7f868216f223973036693
hannaiuia <hannaistudentuia@gmail.com>
2025-02-27 10:54:56 +0100

    mastermind og før lyd flipp flopp

---
+++ module_5/Mastermind/Mastermind.mjs
@@ -0,0 +1,93 @@
+"use strict";
+
+//--------------------------------------------------------------------------------------------------------------------
+//------ Imports
+//--------------------------------------------------------------------------------------------------------------------
+import lib2D from "../../common/libs/lib2d_v2.mjs";
+import libSprite from "../../common/libs/libSprite_v2.mjs";
+import {TColorPicker} from "./ColorPicker.mjs"; 
+import MastermindBoard from "./MastermindBoard.mjs";
+//--------------------------------------------------------------------------------------------------------------------
+//------ Variables, Constants and Objects
+//--------------------------------------------------------------------------------------------------------------------
+
+// prettier-ignore
+export const SpriteInfoList = {
+  Board:              { x: 320, y:   0, width: 441, height: 640, count: 1 },
+  ButtonNewGame:      { x:   0, y:  45, width: 160, height:  45, count: 2 },
+  ButtonCheckAnswer:  { x:   0, y:   0, width: 160, height:  45, count: 2 },
+  ButtonCheat:        { x:   0, y: 139, width:  75, height:  49, count: 2 },
+  PanelHideAnswer:    { x:   0, y:  90, width: 186, height:  49, count: 1 },
+  ColorPicker:        { x:   0, y: 200, width:  34, height:  34, count: 8 },
+  ColorHint:          { x:   0, y: 250, width:  19, height:  18, count: 2 },
+};
+
+const cvs = document.getElementById("cvs");
+const spcvs = new libSprite.TSpriteCanvas(cvs);
+
+
+//Add all you game objects here
+export const GameProps = {
+Board: new libSprite.TSprite(spcvs, SpriteInfoList.Board, new lib2D.TPoint(0,0)), 
+snapTo: {
+  positions: MastermindBoard.ColorAnswer.Row1, 
+  distance: 20
+}, 
+ColorPicker: null,
+}; 
+
+//--------------------------------------------------------------------------------------------------------------------
+//------ Functions
+//--------------------------------------------------------------------------------------------------------------------
+
+function newGame() {
+}
+
+function drawGame(){
+  spcvs.clearCanvas();
+  GameProps.Board.draw();  
+  for(let i = 0; i <GameProps.ColorPicker.length; i++){
+    const colorPicker = GameProps.ColorPicker[i]; 
+    colorPicker.draw(); 
+  }
+
+  //Draw all game objects here, remember to think about the draw order (layers in PhotoShop for example!)
+  
+  requestAnimationFrame(drawGame);
+}
+
+//--------------------------------------------------------------------------------------------------------------------
+//------ Event Handlers
+//--------------------------------------------------------------------------------------------------------------------
+
+//loadGame runs once when the sprite sheet is loaded
+function loadGame() {
+  //Set canvas with and height to match the sprite sheet
+  cvs.width = SpriteInfoList.Board.width;
+  cvs.height = SpriteInfoList.Board.height;
+  spcvs.updateBoundsRect(); 
+
+  GameProps.ColorPicker = [
+    new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Black", 0 ), 
+    new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Blue", 1), 
+    new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Brown", 2 ), 
+    new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Green", 3),
+    new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Orange", 4),
+    new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Red", 5),
+    new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "White", 6),
+    new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Yellow", 7)]
+    
+
+
+  newGame();
+  requestAnimationFrame(drawGame); // Start the animation loop
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------
+//------ Main Code
+//--------------------------------------------------------------------------------------------------------------------
+
+
+spcvs.loadSpriteSheet("./Media/SpriteSheet.png", loadGame);
+window.addEventListener("resize", () => spcvs.updateBoundsRect());
